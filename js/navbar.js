2dfe07f9b0df386609f07b1a323983f4cee62ec7
hoardan <jordanjdamrau@gmail.com>
2021-11-18 03:00:57 -0500

    new dsgn

--- js/navbar.js
+++ js/navbar.js
@@ -1,36 +1,33 @@
-function show() {
-  document.getElementById('sidebar').classList.toggle('active');
+
+// select dom items
+const menuBtn = document.querySelector(".menu-btn");
+
+const menu = document.querySelector(".menu");
+
+const menuNav = document.querySelector(".menu-nav");
+
+const navItems = document.querySelectorAll(".nav-item");
+
+// Set the initial state of the menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    navItems.forEach((item) => item.classList.add("show"));
+
+    // Reset the menu state
+    showMenu = true;
+} else {
+    navItems.forEach((item) => item.classList.remove("show"));
+    menuNav.classList.remove("show");
+    menu.classList.remove("show");
+    menuBtn.classList.remove("close");
+    // Reset the menu state
+    showMenu = false;
 }
-
-window.onscroll = function() {myFunction()};
-// Get the navbar
-var navbar = document.getElementById("sidebar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
 }
-
-$(window).on("load",function() {
-  $(window).scroll(function() {
-    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-    $(".fade").each(function() {
-      /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
-      
-      /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
-      } else { //object goes out of view (scrolling up)
-        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
-      }
-    });
-  }).scroll(); //invoke scroll-handler on page-load
-});
--- js/typewriter.js
+++ js/typewriter.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(event){
   // array with texts to type in typewriter
-  var dataText = [ "New York", "Game Developer.", "Jordan Damrau"];
+  var dataText = [ "New York.", "Software Dev.", "Web Dev.", "Jordan Damrau."];
   
   // type one text in the typwriter
   // keeps calling itself until the text is finished
