cb9d8db080428191c95f49040644b71dc75fcfb2
Jordan Damrau <jordanjdamrau@gmail.com>
2023-02-23 10:05:52 -0500

    trying fix for contact form

--- form_data.php
+++ form_data.php
@@ -1,75 +1,41 @@
 <?php
-if (isset($_POST['Email'])) {
-    
-    $email_to = "jordanjdamrau@gmail.com";
-    $email_subject = "New form submissions";
-
-    function problem($error)
+$action=$_REQUEST['action'];
+if ($action=="")    /* display the contact form */
     {
-        echo "We're sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br><br>";
-        echo $error . "<br><br>";
-        echo "Please go back and fix these errors.<br><br>";
-        die();
-    }
-
-    // validation expected data exists
-    if (
-        !isset($_POST['Name']) ||
-        !isset($_POST['Email']) ||
-        !isset($_POST['Message'])
-    ) {
-        problem('We are sorry, but there appears to be a problem with the form you submitted.');
-    }
-
-    $name = $_POST['Name']; // required
-    $email = $_POST['Email']; // required
-    $message = $_POST['Message']; // required
-
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-
-    if (!preg_match($email_exp, $email)) {
-        $error_message .= 'The Email address you entered does not appear to be valid.<br>';
-    }
-
-    $string_exp = "/^[A-Za-z .'-]+$/";
-
-    if (!preg_match($string_exp, $name)) {
-        $error_message .= 'The Name you entered does not appear to be valid.<br>';
-    }
-
-    if (strlen($message) < 2) {
-        $error_message .= 'The Message you entered do not appear to be valid.<br>';
-    }
-
-    if (strlen($error_message) > 0) {
-        problem($error_message);
-    }
-
-    $email_message = "Form details below.\n\n";
-
-    function clean_string($string)
+    ?>
+    <form id="fcf-form-id" class="fcf-form-class" method="post" action="form_data.php">
+        <br><br>
+        <label for="Name">name (first and last)</label>
+        <input type="text" id="Name" name="Name" required>
+        <br><br><br>    
+        <label for="Email">email</label>
+        <input type="email" id="Email" name="Email" required>
+        <br><br><br>
+        <label for="Phone">phone number</label>
+        <input type="phone" id="Phone" maxlength="8" name="Phone">
+        <br><br><br>
+        <label for="Message">reason</label>
+        <br>
+        <textarea id="Message" name="Message" rows="8" cols="60" required></textarea>
+        <br><br>
+        <button id="submit" name="submit">submit</button>
+    </form>
+    <?php
+    } 
+else                /* send the submitted data */
     {
-        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
-        return str_replace($bad, "", $string);
-    }
-
-    $email_message .= "Name: " . clean_string($name) . "\n";
-    $email_message .= "Email: " . clean_string($email) . "\n";
-    $email_message .= "Message: " . clean_string($message) . "\n";
-
-    // create email headers
-    $headers = 'From: ' . $email . "\r\n" .
-        'Reply-To: ' . $email . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
-    @mail($email_to, $email_subject, $email_message, $headers);
-?>
-
-    <!-- INCLUDE YOUR SUCCESS MESSAGE BELOW -->
-
-    Thanks for getting in touch. We'll get back to you soon.
-
-<?php
-}
+    $name=$_REQUEST['name'];
+    $email=$_REQUEST['email'];
+    $message=$_REQUEST['message'];
+    if (($name=="")||($email=="")||($message==""))
+        {
+        echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+        }
+    else{        
+        $from="From: $name<$email>\r\nReturn-path: $email";
+        $subject="Message sent using your contact form";
+        mail("jordanjdamrau@gmail.com", $subject, $message, $from);
+        echo "Email sent!";
+        }
+    }  
 ?>
